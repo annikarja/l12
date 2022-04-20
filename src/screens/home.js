896f058adca6f216cd3e1dd926845d8e52f65bce
Anni Karja <anni.karja@hotmail.fi>
2022-04-20 09:06:49 +0300

    fixes

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,6 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useTranslation } from "react-i18next";
-import { t } from 'i18next';
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
@@ -14,15 +13,24 @@ const settingsOpts = (opts={lang: 'EN'}) => {
 };
 
 export const Home = ({ navigation, dispatch, lang }) => {
-    const [settings, initSettings] = React.useState(settingsOpts());
-    const { t } = useTranslation();
+    // const [settings, initSettings] = React.useState(settingsOpts());
+    const { t, i18n } = useTranslation();
 
     React.useEffect(() => {
         AsyncStorage.getItem('settings')
-        .then((_settings) => initSettings(JSON.parse(_settings)))
-        .catch(() => initSettings(settingOpts()))
-        .finally(() => dispatch({ type: settings.lang }))
-    }, [])
+        .then((settingsJSON) => {
+            const _settings = settingsOpts(JSON.parse(settingsJSON));
+            console.log({ _settings, settingsJSON });
+            i18n.changeLanguage(_settings.lang);
+            dispatch({ type: _settings.lang });
+        })
+        .catch(() => {
+            const _settings = settingsOpts();
+            console.log({ _settings, settingsJSON });
+            i18n.changeLanguage(_settings.lang);
+            dispatch({ type: _settings.lang });
+        });
+    }, []);
 
     return (<View style={styles.main}>
         <Text>{t('Home')}</Text>
